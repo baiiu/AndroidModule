42884f318f238460d42e887799dc88ee1621e918
baiiu <baiiu@foxmail.com>
2016-06-30 15:36:17 +0800

    filter zip

--- RxJavaLearn/src/main/java/com/example/testing/rxjavalearn/fragments/TestFragment.java
+++ RxJavaLearn/src/main/java/com/example/testing/rxjavalearn/fragments/TestFragment.java
@@ -20,7 +20,51 @@ public class TestFragment extends BaseFragment {
         //        filterTest();
         //        firstTest();
 
-        zipTest();
+        //zipTest();
+        //zipTest2();
+        filterZip();
+    }
+
+    private void filterZip() {
+
+        Observable<Integer> filter = Observable.just(2, 6)
+                .filter(integer -> integer < 2);
+
+        Observable<Integer> just = Observable.just(1);
+
+        Observable.zip(filter, just, (integer, integer2) -> String.valueOf(integer) + ", " + integer2)
+                .subscribe(getSubscriber());
+
+    }
+
+    private void zipTest2() {
+
+
+        Observable<Integer> just = Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override public void call(Subscriber<? super Integer> subscriber) {
+                subscriber.onError(new IllegalStateException("报个错"));
+            }
+        });
+
+        Observable<String> string1 = Observable.just("String1");
+
+
+        Observable.zip(just, string1, new Func2<Integer, String, String>() {
+            @Override public String call(Integer integer, String s) {
+                return String.valueOf(integer) + ", " + s;
+            }
+        })
+                .onErrorResumeNext(throwable -> {
+                    //直接走到null,不会对单个接口处理
+                    return Observable.just(null);
+
+                    //拦截onError
+                    //return Observable.just(22);
+
+                    //使用这个时直接调用onComplete(),onNext()并不会走
+                    //return Observable.empty();
+                })
+                .subscribe(getSubscriber());
     }
 
     private void zipTest() {
@@ -31,8 +75,11 @@ public class TestFragment extends BaseFragment {
             }
         })
                 .onErrorResumeNext(throwable -> {
+
+                    return Observable.just(null);
+
                     //拦截onError
-                    return Observable.just(22);
+                    //return Observable.just(22);
 
                     //使用这个时直接调用onComplete(),onNext()并不会走
                     //return Observable.empty();
@@ -46,19 +93,7 @@ public class TestFragment extends BaseFragment {
                 return String.valueOf(integer) + ", " + s;
             }
         })
-                .subscribe(new Subscriber<String>() {
-                    @Override public void onCompleted() {
-                        LogUtil.d("onCompleted");
-                    }
-
-                    @Override public void onError(Throwable e) {
-                        LogUtil.d(e.toString());
-                    }
-
-                    @Override public void onNext(String s) {
-                        LogUtil.d(s);
-                    }
-                });
+                .subscribe(getSubscriber());
 
     }
 
