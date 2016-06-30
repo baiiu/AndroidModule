f2aa8fb8f667224151fa128f5926b24644ee3ea2
baiiu <baiiu@foxmail.com>
2016-06-30 14:54:04 +0800

    test onErrorResumeNext

--- RxJavaLearn/src/main/java/com/example/testing/rxjavalearn/fragments/TestFragment.java
+++ RxJavaLearn/src/main/java/com/example/testing/rxjavalearn/fragments/TestFragment.java
@@ -1,10 +1,8 @@
 package com.example.testing.rxjavalearn.fragments;
 
 import android.os.Bundle;
-
 import com.example.testing.rxjavalearn.operators.BaseFragment;
 import com.example.testing.rxjavalearn.util.LogUtil;
-
 import rx.Observable;
 import rx.Subscriber;
 
@@ -15,12 +13,45 @@ import rx.Subscriber;
  */
 public class TestFragment extends BaseFragment {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+    @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        filterTest();
-        firstTest();
+        //        filterTest();
+        //        firstTest();
+
+        zipTest();
+    }
+
+    private void zipTest() {
+
+        Observable<Integer> just = Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override public void call(Subscriber<? super Integer> subscriber) {
+                subscriber.onError(new IllegalStateException("报个错"));
+            }
+        })
+                .onErrorResumeNext(throwable -> {
+                    //拦截onError
+                    return Observable.just(22);
+                });
+
+        Observable<String> string1 = Observable.just("String1");
+
+
+        Observable.zip(just, string1, (integer, s) -> String.valueOf(integer) + ", " + s)
+                .subscribe(new Subscriber<String>() {
+                    @Override public void onCompleted() {
+                        LogUtil.d("onCompleted");
+                    }
+
+                    @Override public void onError(Throwable e) {
+                        LogUtil.d(e.toString());
+                    }
+
+                    @Override public void onNext(String s) {
+                        LogUtil.d(s);
+                    }
+                });
+
     }
 
     /**
@@ -39,27 +70,23 @@ public class TestFragment extends BaseFragment {
      * filter reture false后直接回调onComplete,不会走onError.
      */
     private void filterTest() {
-        Integer[] ints = new Integer[]{1, 2, 3, 4};
+        Integer[] ints = new Integer[] { 1, 2, 3, 4 };
 
         Observable.from(ints)
                 .filter(integer -> integer < 3)
                 .subscribe(new Subscriber<Integer>() {
-                    @Override
-                    public void onCompleted() {
+                    @Override public void onCompleted() {
                         LogUtil.d("onComplete");
                     }
 
-                    @Override
-                    public void onError(Throwable e) {
+                    @Override public void onError(Throwable e) {
                         LogUtil.e(e.toString());
                     }
 
-                    @Override
-                    public void onNext(Integer integer) {
+                    @Override public void onNext(Integer integer) {
                         LogUtil.d(integer);
                     }
                 });
-
 
     }
 }
