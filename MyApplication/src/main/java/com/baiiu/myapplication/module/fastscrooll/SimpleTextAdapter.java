cc3587aeea2b7707b1b93ebb963856635726191a
baiiu <baiiu@foxmail.com>
2016-08-02 16:43:27 +0800

    rename

---
+++ MyApplication/src/main/java/com/baiiu/myapplication/module/fastscrooll/SimpleTextAdapter.java
@@ -0,0 +1,49 @@
+package com.baiiu.myapplication.module.fastscrooll;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * author: baiiu
+ * date: on 16/7/1 19:47
+ * description:
+ */
+public class SimpleTextAdapter extends RecyclerView.Adapter<SimpleTextViewHolder> {
+
+    private Context mContext;
+    private int mCount;
+
+    public SimpleTextAdapter(Context context, int count) {
+        mContext = context;
+        mCount = count;
+    }
+
+    @Override public SimpleTextViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new SimpleTextViewHolder(mContext, parent);
+    }
+
+    @Override public void onBindViewHolder(SimpleTextViewHolder holder, int position) {
+        holder.bind(position);
+    }
+
+    @Override public int getItemCount() {
+        return mCount;
+    }
+
+    public void setCount(int mCount) {
+        notifyItemRangeInserted(this.mCount, 20);
+        this.mCount = mCount;
+    }
+
+    public void addOne(int position) {
+        notifyItemInserted(position);
+        ++this.mCount;
+    }
+
+    public void deleteOne(int position) {
+        notifyItemRemoved(position);
+        --this.mCount;
+    }
+
+}
---
+++ MyApplication/src/main/java/com/baiiu/myapplication/module/fastscrooll/SimpleTextViewHolder.java
@@ -0,0 +1,40 @@
+package com.baiiu.myapplication.module.fastscrooll;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * author: baiiu
+ * date: on 16/6/27 17:38
+ * description:
+ */
+public class SimpleTextViewHolder extends RecyclerView.ViewHolder {
+
+    public SimpleTextViewHolder(Context context, ViewGroup parent) {
+        super(LayoutInflater.from(context)
+                      .inflate(android.R.layout.simple_list_item_1, parent, false));
+
+        itemView.setPadding(0, 100, 0, 100);
+
+    }
+
+    public void bind(int position) {
+
+        ((TextView) itemView).setText("position: " + String.valueOf(position));
+
+
+        if (position % 2 == 0) {
+            itemView.setBackgroundResource(android.R.color.darker_gray);
+        } else {
+            itemView.setBackgroundResource(android.R.color.background_light);
+        }
+    }
+
+    public void bind(String text) {
+        ((TextView) itemView).setText(text);
+    }
+
+}
