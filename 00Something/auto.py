b157bbec0af896c0b8415a22b1014eaebbc25a5a
baiiu <baiiu@foxmail.com>
2016-08-06 12:30:58 +0800

    add auto assemble apk with python

--- 00Something/auto.py
+++ 00Something/auto.py
@@ -1,4 +1,141 @@
-import os;
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+# import shutil
 
-os.system("cd Desktop/")
-os.system("git clone https://github.com/baiiu/DropDownMenu.git");
+
+# 1.设置目录
+base_file_dir = '/Users/baiiu/Desktop'
+create_dir_name='AndroidApp'
+create_code_dir_name = 'SourceCode'
+create_apk_dir_name = 'Apk'
+
+file_dir = base_file_dir + '/' + create_dir_name    #/Users/baiiu/Desktop/AndroidApp
+code_dir = file_dir + '/' + create_code_dir_name      #/Users/baiiu/Desktop/AndroidApp/SourceCode
+apk_dir = file_dir + '/' + create_apk_dir_name      #/Users/baiiu/Desktop/AndroidApp/Apk
+
+git_clone_address = "https://github.com/baiiu/ZhihuDaily.git";
+
+##########################################################################################
+#2. 在桌面上创建目录
+print ('现在所在位置： ' + os.getcwd())
+os.chdir(base_file_dir);
+print ('进入桌面： ' + os.getcwd())
+
+print('\n')
+print('=============================================')
+print('create dirs')
+print('=============================================')
+
+if not os.path.exists(file_dir):
+    os.mkdir(create_dir_name)
+
+os.chdir(file_dir)
+print('进入AndroidApp： ' + os.getcwd())
+
+if not os.path.exists(code_dir):
+    os.mkdir(create_code_dir_name)
+
+if not os.path.exists(apk_dir):
+    os.mkdir(create_apk_dir_name)
+
+##########################################################################################
+#3. 执行git clone命令
+
+# 进入SourceCode目录下
+os.chdir(code_dir)
+print('进入SourceCode下： ' + os.getcwd())
+
+print('\n')
+print('=============================================')
+print('git clone or git pull')
+print('=============================================')
+
+if not os.listdir(code_dir):
+    #空文件夹
+    os.system('git clone ' + git_clone_address +' ' + code_dir)
+else:
+    #已经clone过
+    os.system('git pull')
+
+##########################################################################################
+#4. gradle assemble打包命令，需要生成local.properties文件
+def createLocalPropertiesFile(sourceDir,fileName):
+    if not os.path.exists(sourceDir):
+        return
+
+    fileDir = sourceDir + '/' + fileName
+
+    if os.path.exists(fileDir):
+        return
+
+    f = open(fileDir,'w');
+    f.write('sdk.dir=/Users/baiiu/Library/Android/sdk')
+    f.close()
+
+# 生成local.properties文件
+createLocalPropertiesFile(code_dir,'local.properties')
+
+# 打包
+print('\n')
+print('=============================================')
+print('gradle clean')
+print('=============================================')
+os.system('gradle clean');
+
+print('\n')
+print('=============================================')
+print('gradle assembleDebug, generate apk')
+print('=============================================')
+os.system('gradle assembleDebug')
+
+##########################################################################################
+#5.移动.apk文件到Apk目录，方便查找
+def moveFiles(sourceDir,  targetDir):#复制一级目录下的所有文件到指定目录
+    if not os.path.exists(sourceDir):
+        return;
+
+    for file in os.listdir(sourceDir):
+         sourceFile = os.path.join(sourceDir,  file)
+         targetFile = os.path.join(targetDir,  file)
+         if os.path.isfile(sourceFile) and ('unaligned' not in os.path.basename(sourceFile)):
+             open(targetFile,"wb").write(open(sourceFile,"rb").read())
+
+source_apk_dir = code_dir + '/' + 'app/build/outputs/apk'
+if os.path.exists(source_apk_dir):
+    moveFiles(source_apk_dir,apk_dir)
+
+
+##########################################################################################
+#6. 安装apk
+print('\n')
+print('=============================================')
+print('install apk')
+print('=============================================')
+
+print ('现在所在位置： ' + os.getcwd())
+
+
+def getFileName(sourceDir):
+    if not os.path.exists(sourceDir):
+        return;
+
+    for filename in os.listdir(sourceDir):
+        if '.apk' in filename:
+            return filename
+
+
+
+apkName = getFileName(apk_dir)
+apkPath = apk_dir + '/' + apkName
+print(apkPath)
+
+os.system('adb install -r ' + apkPath)
+
+##########################################################################################
+# 7.打开APK
+
+# 先关闭该进程
+os.system('adb shell am force-stop com.baiiu.zhihudaily')
+# 打开该A
+os.system('adb shell am start -n com.baiiu.zhihudaily/com.baiiu.zhihudaily.newsList.view.NewsListActivity')
---
+++ 00Something/learnPython.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+
+
+
+def moveFiles(sourceDir,  targetDir):#复制一级目录下的所有文件到指定目录
+    if not os.path.exists(sourceDir):
+        return;
+
+    for file in os.listdir(sourceDir):
+         sourceFile = os.path.join(sourceDir,  file)
+         targetFile = os.path.join(targetDir,  file)
+         if os.path.isfile(sourceFile) and ('unaligned' not in os.path.basename(sourceFile)):
+                open(targetFile,"wb").write(open(sourceFile,"rb").read())
+
+
+code_dir = '/Users/baiiu/Desktop/AndroidApp/SourceCode'
+source_apk_dir = code_dir + '/' + 'app/build/outputs/apk'
+
+moveFiles(source_apk_dir,'/Users/baiiu/Desktop/AndroidApp/Apk')
+
+
+
+def createLocalPropertiesFile(sourceDir,fileName):
+    if not os.path.exists(sourceDir):
+        return
+
+    fileDir = sourceDir + '/' + fileName
+
+    if os.path.exists(fileDir):
+        print('已经存在该文件了')
+        return
+
+    print('create file:')
+    f = open(fileDir,'w');
+    f.write('sdk.dir=/Users/baiiu/Library/Android/sdk')
+    f.close()
+
+
+# createLocalPropertiesFile('/Users/baiiu/Desktop/test','local.properties')
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# # 1. 设置路径
+# file_dir = "/Users/baiiu/Desktop"
+#
+# #2. 测试一个文件
+# file_name = "test.txt"
+#
+# #3. 连接起来，全路径，指向一个文件
+# file_abs = os.path.join(file_dir, file_name)
+#
+#
+# # 判断是否绝对路径
+# print(1,os.path.isabs(file_dir))
+# print (2,os.path.isabs(file_name))
+# print (3,os.path.isabs(file_abs))
+#
+# # 判断是否真实存在
+# print (4,os.path.exists(file_abs))
+# print (4,os.path.exists(file_dir))
+# print (5,os.path.exists(os.path.join(file_dir,"xxx")))
+#
+# # 判断是否是个目录
+# print (6,os.path.isdir(file_dir))
+# print (7,os.path.isdir(file_abs))
+#
+# # 判断是否是个文件
+# print (8,os.path.isfile(file_dir))
+# print (9,os.path.isfile(file_abs))
+#
+
+
+
+
+
+
+
+
+
+
+# file_abs = "/Users/baiiu/";
+#
+#
+# if()
+# os.mkdir("test");
+#
+# os.chdir(file_abs+'/test');
+#
+# f = open("a.txt","w+");
+# print(f.name);
+
+
+
+# os.system("cd Desktop/")
+# os.system("git clone https://github.com/baiiu/DropDownMenu.git");
+#
+#
+# print(os.name);
+
+
+# 查看当前目录的绝对路径:
+# >>> os.path.abspath('.')
+# '/Users/michael'
+# 在某个目录下创建一个新目录，首先把新目录的完整路径表示出来:
+# >>> os.path.join('/Users/michael', 'testdir')
+# '/Users/michael/testdir'
+# 然后创建一个目录:
+# >>> os.mkdir('/Users/michael/testdir')
+# 删掉一个目录:
+# >>> os.rmdir('/Users/michael/testdir')
