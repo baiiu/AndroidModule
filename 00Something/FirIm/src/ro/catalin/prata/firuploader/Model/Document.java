e339a2814d5a4787373b1a46781c69e22e00edc6
baiiu <baiiu@foxmail.com>
2016-08-13 00:38:25 +0800

    easy upto fir.im

---
+++ 00Something/FirIm/src/ro/catalin/prata/firuploader/Model/Document.java
@@ -0,0 +1,61 @@
+package ro.catalin.prata.firuploader.Model;
+
+import ro.catalin.prata.firuploader.utils.Utils;
+
+/**
+ * Created with IntelliJ IDEA. User: will Date: 15/8/8 Time: 上午12:21 做中英话处理的类
+ */
+public class Document {
+	public String formHeader;
+	public String formToken;
+	public String formProject;
+	public String formPath;
+	public String formLog;
+	public String formLink;
+	public String setTokenBtn;
+	public String settingBtn;
+	public String uploadBtn;
+	public String formTip;
+	public String formUpload;
+	public String cancelUpload;
+	public String languageLabel;
+	public String chineseBtn;
+	public String englishBtn;
+
+	public Document() {
+		if (Utils.isZh()) {
+			formHeader = "fir.im 一键上传";
+			formToken = "api_token";
+			formProject = "项目名称";
+			formPath = "文件路径";
+			formLog = "更新日志";
+			formLink = "应用地址";
+			setTokenBtn = "设置";
+			settingBtn = "选择文件";
+			uploadBtn = "上传";
+			formTip = "检测提示";
+			formUpload = "自动上传";
+			cancelUpload = "取消上传";
+			languageLabel = "语言";
+			chineseBtn = "中文";
+			englishBtn = "英文";
+		} else {
+			formHeader = "fir.im upload";
+			formToken = "api_token";
+			formProject = "Project";
+			formPath = "File path";
+			formLog = "Changelog";
+			formLink = "Short";
+			setTokenBtn = "Setting";
+			settingBtn = "Choose path";
+			uploadBtn = "Upload";
+			formTip = "Check & tip";
+			formUpload = "Auto upload";
+			cancelUpload = "cancel upload";
+			languageLabel = "Language";
+			chineseBtn = "Chinese";
+			englishBtn = "English";
+		}
+
+	}
+}
---
+++ 00Something/FirIm/src/ro/catalin/prata/firuploader/utils/UploadToRio.java
@@ -0,0 +1,121 @@
+package ro.catalin.prata.firuploader.utils;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import ro.catalin.prata.firuploader.Model.UploadTicket;
+
+/**
+ * Created with IntelliJ IDEA. User: will Date: 15/8/7 Time: 下午1:48 To change
+ * this template use File | Settings | File Templates.
+ */
+public class UploadToRio {
+	public static final String FIR_UPLOAD_TOKEN_URL = "http://fir.im/api/v2/info/";
+	public static final String FIR_UPDATE_APP_INFO = "http://fir.im/api/v2/app/";
+	public static final String FIR_BASE_URL = "http://api.fir.im";
+
+	public void setVersionId(String versionId) {
+		this.versionId = versionId;
+	}
+
+	public String versionId;
+	public String token;
+	public String appName;
+	public String appId;
+	public String versionName;
+	public String versionCode;
+	public String changeLog;
+	public String appShort;
+	public String appOid;
+	public UploadTicket uploadTicket;
+
+	public UploadToRio(String appId, String token, String appName, String versionName, String versionCode,
+			String changeLog) {
+		this.appId = appId;
+		this.appName = appName;
+		this.versionCode = versionCode;
+		this.changeLog = changeLog;
+		this.versionName = versionName;
+		this.token = token;
+
+		uploadTicket = new UploadTicket(getUploadToken());
+	}
+
+	/**
+	 * 获取上传token
+	 * 
+	 * @return JSONObject
+	 */
+	public JSONObject getUploadToken() {
+		try {
+			HttpClient httpClient = new DefaultHttpClient();
+			String url = FIR_BASE_URL + "/apps";
+			String type = "android";
+			HttpPost post = new HttpPost(url);
+			List<NameValuePair> parameters;
+
+			ArrayList<BasicNameValuePair> postParameters = new ArrayList<BasicNameValuePair>();
+			postParameters.add(new BasicNameValuePair("type", type));
+			postParameters.add(new BasicNameValuePair("bundle_id", this.appId));
+			postParameters.add(new BasicNameValuePair("api_token", this.token));
+			post.setEntity(new UrlEncodedFormEntity(postParameters, "UTF-8"));
+			HttpResponse response = null;
+			response = httpClient.execute(post);
+			HttpEntity entity = response.getEntity();
+			String responseString = EntityUtils.toString(entity, "UTF-8");
+
+			System.out.println(responseString);
+			JSONObject jo;
+			jo = new JSONObject(responseString);
+			return jo;
+
+		} catch (UnsupportedEncodingException e) {
+			Utils.postErrorNoticeTOSlack(e);
+			e.printStackTrace(); // To change body of catch statement use File |
+									// Settings | File Templates.
+		} catch (IOException e) {
+			Utils.postErrorNoticeTOSlack(e);
+			e.printStackTrace(); // To change body of catch statement use File |
+									// Settings | File Templates.
+		} catch (JSONException e) {
+			Utils.postErrorNoticeTOSlack(e);
+			e.printStackTrace(); // To change body of catch statement use File |
+									// Settings | File Templates.
+		}
+		return new JSONObject();
+	}
+
+	/**
+	 * 上传Binary
+	 * 
+	 * @return
+	 */
+	public JSONObject uploadBinary() {
+
+		return new JSONObject();
+	}
+
+	/**
+	 * 上传icon
+	 * 
+	 * @return
+	 */
+	public JSONObject uploadIcon() {
+		return new JSONObject();
+	}
+
+}
