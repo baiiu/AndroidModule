8ddf5cf599aa7dd73278b4a769036236cb0617b6
baiiu <baiiu@foxmail.com>
2016-10-25 10:03:00 +0800

    add a Java Test lib

---
+++ JavaTest/src/main/java/com/example/Child.java
@@ -0,0 +1,12 @@
+package com.example;
+
+/**
+ * author: baiiu
+ * date: on 16/10/25 10:01
+ * description:
+ */
+
+public class Child extends Father {
+
+
+}
---
+++ JavaTest/src/main/java/com/example/Father.java
@@ -0,0 +1,16 @@
+package com.example;
+
+/**
+ * author: baiiu
+ * date: on 16/10/25 10:00
+ * description:
+ */
+
+public class Father {
+
+    public static void OneMethod() {
+        System.out.println("oneMethod");
+    }
+
+
+}
