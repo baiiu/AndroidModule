c686dd6aba8bece1afdfd2de343086cdb6d406d7
baiiu <baiiu@foxmail.com>
2016-10-25 09:56:34 +0800

    add a package library

---
+++ Packaged/LogUtilRelease/src/main/java/com/baiiu/library/LogUtil.java
@@ -0,0 +1,87 @@
+package com.baiiu.library;
+
+/**
+ * LogUtilRelease,减少包体积
+ */
+public class LogUtil {
+    public static void init(boolean isShowLog) {
+    }
+
+    public static void init(boolean isShowLog, String tag) {
+    }
+
+    public static void v() {
+    }
+
+    public static void v(Object msg) {
+    }
+
+    public static void v(String tag, Object... objects) {
+    }
+
+    public static void d() {
+    }
+
+    public static void d(Object msg) {
+    }
+
+    public static void d(String tag, Object... objects) {
+    }
+
+    public static void i() {
+    }
+
+    public static void i(Object msg) {
+    }
+
+    public static void i(String tag, Object... objects) {
+    }
+
+    public static void w() {
+    }
+
+    public static void w(Object msg) {
+    }
+
+    public static void w(String tag, Object... objects) {
+    }
+
+    public static void e() {
+    }
+
+    public static void e(Object msg) {
+    }
+
+    public static void e(String tag, Object... objects) {
+    }
+
+    //public static void a() {
+    //}
+
+    //public static void a(Object msg) {
+    //}
+
+    //public static void a(String tag, Object... objects) {
+    //}
+
+    //public static void json(String jsonFormat) {
+    //}
+    //
+    //public static void json(String tag, String jsonFormat) {
+    //}
+
+    //public static void xml(String xml) {
+    //}
+
+    //public static void xml(String tag, String xml) {
+    //}
+
+    //public static void file(File targetDirectory, Object msg) {
+    //}
+
+    //public static void file(String tag, File targetDirectory, Object msg) {
+    //}
+
+    //public static void file(String tag, File targetDirectory, String fileName, Object msg) {
+    //}
+}
---
+++ Packaged/Stetho/src/main/java/com/facebook/stetho/Stetho.java
@@ -0,0 +1,15 @@
+package com.facebook.stetho;
+
+import android.content.Context;
+
+/**
+ * author: baiiu
+ * date: on 16/10/24 16:54
+ * description:
+ */
+public class Stetho {
+
+    public static void initializeWithDefaults(final Context context) {
+    }
+
+}
