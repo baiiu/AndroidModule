6a79311d9b9ec3d2df2e36526f2c424e701e4878
baiiu <baiiu@foxmail.com>
2020-01-08 12:35:54 +0800

    jni c call java

--- jni/jnitest/src/main/java/com/baiiu/jnitest/MainActivity.java
+++ jni/jnitest/src/main/java/com/baiiu/jnitest/MainActivity.java
@@ -1,5 +1,7 @@
 package com.baiiu.jnitest;
 
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -13,7 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((TextView) findViewById(R.id.textView)).setText(stringFromJNI());
+
+        testCallJava(this);
     }
 
     public native String stringFromJNI();
+
+    private int code = 10;
+    private String msg = "hello world";
+
+    public void cCallJava(String str) {
+        android.util.Log.e("mLogU", "cCallJava: " + str);
+
+        Toast.makeText(this, str, Toast.LENGTH_SHORT)
+                .show();
+    }
+
+
+    public native void testCallJava(MainActivity activity);
+
 }
