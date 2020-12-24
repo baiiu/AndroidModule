70980ddf25d7d6bfb3c6cfbb4ee4ec6e7d3d3249
baiiu <baiiu@foxmail.com>
2020-12-24 15:28:06 +0800

    sth

--- jni/jnitest/src/main/cpp/testSocket/SocketTest.cpp
+++ jni/jnitest/src/main/cpp/testSocket/SocketTest.cpp
@@ -19,7 +19,8 @@ JNIEXPORT jboolean JNICALL
 Java_com_baiiu_jnitest_testSocket_TestSocketFragment_native_1connect(JNIEnv *env, jobject thiz) {
     __android_log_print(ANDROID_LOG_ERROR, "mLogU", "start connect: %p， %p", &env, env);
 
-    int result = function(1000000000);
+//    int result = function(1000000000);
+    int result = function(5);
 
     __android_log_print(ANDROID_LOG_ERROR, "mLogU", "end connect: %p， %p, %d", &env, env, result);
     return true;
--- jni/jnitest/src/main/java/com/baiiu/jnitest/testSocket/TestSocketFragment.java
+++ jni/jnitest/src/main/java/com/baiiu/jnitest/testSocket/TestSocketFragment.java
@@ -2,6 +2,10 @@ package com.baiiu.jnitest.testSocket;
 
 import com.baiiu.jnitest.base.BaseFragment;
 
+import java.lang.reflect.Field;
+
+import dalvik.system.BaseDexClassLoader;
+
 /**
  * author: baiiu
  * time: 2020/12/16
@@ -11,6 +15,27 @@ public class TestSocketFragment extends BaseFragment implements Runnable {
 
     static {
         System.loadLibrary("socket-lib");
+
+
+        try {
+            Field pathListF = BaseDexClassLoader.class.getDeclaredField("pathList");
+            pathListF.setAccessible(true);
+            ClassLoader dexPathClassLoader = TestSocketFragment.class.getClassLoader();
+            Object pathList = pathListF.get(dexPathClassLoader);
+
+            Field nativeLibraryPathElementsF = Class.forName("dalvik.system.DexPathList").getDeclaredField("nativeLibraryPathElements");
+            nativeLibraryPathElementsF.setAccessible(true);
+            Object nativeLibraryPathElements = nativeLibraryPathElementsF.get(pathList);
+            android.util.Log.e("mLogU", nativeLibraryPathElements.toString());
+
+            Object[] arr = (Object[]) nativeLibraryPathElements;
+            for (Object o : arr) {
+                android.util.Log.e("mLogU", o.toString());
+            }
+
+        } catch (Exception e) {
+            android.util.Log.e("mLogU", e.toString());
+        }
     }
 
     private Thread thread;
